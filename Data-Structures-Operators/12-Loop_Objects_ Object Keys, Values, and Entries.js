f4d1976145b0adb350cb32815f16a8afff71d983
HASSAN5077 <talhashafiqch@gmail.com>
2022-12-28 19:57:56 +0500

    js

---
+++ Data-Structures-Operators/12-Loop_Objects_ Object Keys, Values, and Entries.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+// Looping Objects: Object Keys, Values, and Entries
+
+/*
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+*/
+/////////////////////////////////////
+
+/*
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+*/
+/////////////////////////////////////
+
+/*
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+*/
