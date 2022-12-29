35ebae5227e381ce10ddd25bd543522b1aa3dd05
HASSAN5077 <talhashafiqch@gmail.com>
2022-12-29 22:17:27 +0500

    js

---
+++ Closer Look at function/4-returning_functions.js
@@ -0,0 +1,17 @@
+'use strict';
+// Functions Returning Functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Jonas');
