fbb02ffbabe3ae8214ea9ca82696a0aeef3bfa97
HASSAN5077 <talhashafiqch@gmail.com>
2023-01-01 11:38:37 +0500

    js

---
+++ working with Array/12-find_Method.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// The find Method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const account1 = {
+  owner: 'Talha Ch',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Shafiq Ch',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Haseeb Ch',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Zameer Ch',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(acc => acc.owner === 'Talha Ch');
+console.log(account);
