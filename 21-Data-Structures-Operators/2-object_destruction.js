8aafe0a837c94da5b07191a044b960ed91e1617d
HASSAN5077 <talhashafiqch@gmail.com>
2023-01-25 18:04:26 +0500

    js

---
+++ 21-Data-Structures-Operators/2-object_destruction.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const resturant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  openingHours: {
+    Tue: {
+      open: 12,
+      close: 22,
+    },
+    Fri: {
+      open: 11,
+      close: 23,
+    },
+    Sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+//////////////////////////////////
+/*
+resturant.orderDelivery({
+  address: 'Lahore',
+});
+*/
+//////////////////////////////////
+
+/*
+const { name, openingHours, categories } = resturant;
+console.log(name, openingHours, categories);
+*/
+//////////////////////////////////
+
+/*
+const {
+  name: resturantName,
+  openingHours: hours,
+  categories: categ,
+} = resturant;
+console.log(resturantName, hours, categ);
+*/
+//////////////////////////////////
+
+/*
+const { menu = [], starterMenu: starter = [] } = resturant;
+console.log(menu, starter);
+*/
+//////////////////////////////////
+
+/*
+let a = 134;
+let b = 456;
+const obj = { a: 12, b: 13, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+*/
+//////////////////////////////////
+
+/*
+const { openingHours } = resturant;
+const { Fri } = openingHours;
+console.log(Fri);
+*/
+//////////////////////////////////
+
+/*
+const { openingHours } = resturant;
+const {
+  Fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/
---
+++ 21-Data-Structures-Operators/3-spread_operators.js
@@ -0,0 +1,82 @@
+'use strict';
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+};
+
+////////////////////////////////
+/*
+const ingrediants = [
+  prompt("Let's Make pasta! Ingredient 1?"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3?'),
+];
+console.log(ingrediants);
+restaurant.orderPasta(ingrediants[0], ingrediants[1], ingrediants[2]);
+restaurant.orderPasta(...ingrediants);
+*/
+////////////////////////////////
+
+/*
+const newResturant = { foundedIn: 2002, ...restaurant, founder: 'Talha' };
+console.log(newResturant);
+*/
+////////////////////////////////
+
+/*
+const copyResturant = { ...restaurant };
+copyResturant.name = 'Talha';
+console.log(copyResturant.name);
+console.log(restaurant.name);
+*/
+////////////////////////////////
+
+/*
+const arr = [1, 2, 3];
+const bandNewArray = [4, 5, arr[0], arr[1], arr[2]];
+console.log(bandNewArray);
+*/
+////////////////////////////////
+
+/*
+const arr1 = [1, 2, 3];
+const NewArray = [4, 5, arr1];
+console.log(NewArray);
+*/
+////////////////////////////////
+
+/*
+const arr2 = [1, 2, 3];
+const NewArr = [4, 5, ...arr2];
+console.log(NewArr);
+console.log(...NewArr);
+*/
+////////////////////////////////
+
+/*
+const newMenu = [...restaurant.mainMenu, 'Burger'];
+console.log(newMenu);
+*/
+////////////////////////////////
+
+/*
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+*/
+////////////////////////////////
+
+/*
+// iterables:array,string,map,set but not object
+const str = 'Talha';
+const letter = [...str, ' ', 'S.'];
+console.log(letter);
+console.log(...str);
+*/
